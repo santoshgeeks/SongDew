606dcf8299a5a112b818f62a299e95fba83adf1a
Santosh Kumar <santoshrai526@gmail.com>
2022-02-19 16:18:35 +0530

    upadeted path

--- src/app/promotion-home/promotion-home.component.ts
+++ src/app/promotion-home/promotion-home.component.ts
@@ -10,7 +10,7 @@ export class PromotionHomeComponent implements OnInit {
   listOfUploadedMusic=[
     {
       songName: "The Idea Of Her",
-      songImage: "../../assets/image/Musigimage.PNG",
+      songImage: "/assets/image/Musigimage.PNG",
       publishedOn: "Jul 1, 2021",
       noOfPlays: 0,
       disc:"Shitalchandra Kulkarni",
@@ -18,7 +18,7 @@ export class PromotionHomeComponent implements OnInit {
     },
     {
       songName: "The Idea Of Her",
-      songImage: "../../assets/image/Musigimage.PNG",
+      songImage: "/assets/image/Musigimage.PNG",
       publishedOn: "Jul 1, 2021",
       noOfPlays: 0,
       disc:"Shitalchandra Kulkarni",
@@ -26,7 +26,7 @@ export class PromotionHomeComponent implements OnInit {
     },
     {
       songName: "The Idea Of Her",
-      songImage: "../../assets/image/Musigimage.PNG",
+      songImage: "/assets/image/Musigimage.PNG",
       publishedOn: "Jul 1, 2021",
       noOfPlays: 0,
       disc:"Shitalchandra Kulkarni",
@@ -34,7 +34,7 @@ export class PromotionHomeComponent implements OnInit {
     },
     {
       songName: "The Idea Of Her",
-      songImage: "../../assets/image/Musigimage.PNG",
+      songImage: "/assets/image/Musigimage.PNG",
       publishedOn: "Jul 1, 2021",
       noOfPlays: 0,
       disc:"Shitalchandra Kulkarni",
@@ -42,7 +42,7 @@ export class PromotionHomeComponent implements OnInit {
     },
     {
       songName: "The Idea Of Her",
-      songImage: "../../assets/image/Musigimage.PNG",
+      songImage: "/assets/image/Musigimage.PNG",
       publishedOn: "Jul 1, 2021",
       noOfPlays: 0,
       disc:"Shitalchandra Kulkarni",
@@ -50,7 +50,7 @@ export class PromotionHomeComponent implements OnInit {
     },
     {
       songName: "The Idea Of Her",
-      songImage: "../../assets/image/Musigimage.PNG",
+      songImage: "/assets/image/Musigimage.PNG",
       publishedOn: "Jul 1, 2021",
       noOfPlays: 0,
       disc:"Shitalchandra Kulkarni",
